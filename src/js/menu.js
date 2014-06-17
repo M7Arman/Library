c2ede6d2eceda3acea3ef60eb01951b32887da6e
M7Arman <armankostandyan@gmail.com>
2014-06-17 09:59:10 +0400

    modify src/menu.js and src/index.html (added http reguest)

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,18 @@
+var app = angular.module("app", []);
 
+app.controller("AppCtrl", function ($http) {
+	$http.get("http://localhost/library/contact.html")
+		.success(function (data) {
+			console.log(data)
+		})
+/* motavor 2 ropeic		
+*https://egghead.io/lessons/angularjs-http
+*	$http.post("http://localhost/library/contact.html")
+*		.success(function (data) {
+*			console.log(data)
+*		})
+*})
+*/
 function menuCtrl($scope) {
 
  $scope.lastClick = "contact";
