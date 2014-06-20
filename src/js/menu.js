ee8dc90d237a1e1fe9ea2dadd05955566273be25
Seryozha <serojkhachatryan95.com>
2014-06-20 16:41:26 +0400

    Modify request, html

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,4 @@
 function menuCtrl($scope, $http) {
-	$scope.lastClick = "contact";
 	$scope.menuList = {
 		home: true,
 		contact: false,
@@ -10,8 +9,10 @@ function menuCtrl($scope, $http) {
 		contact: 'page/contact.html',
 		aboutAs: 'page/aboutAs.html',
 		addUser: 'page/addUser.html',
-		searchResponse: 'page/search.html'
+		searchData: 'page/search.html',
+		categoryData: 'page/category.html',
 	}
+
 	$scope.currentPath = $scope.paths.home;
 
 	$scope.homeClick = function() {
@@ -46,22 +47,53 @@ function menuCtrl($scope, $http) {
 	$scope.getPath = function() {
 		return $scope.currentPath;
 	};
-	
- 	console.log($scope.searchText);
- 	$scope.runSearch = function() {
- 	$scope.clearTrue();
- 	$scope.currentPath = $scope.paths.searchResponse;
-	console.log($scope.searchText);
-	$scope.url = 'http://localhost:8000/library/bookSearch?callback=JSON_CALLBACK&bookName='+$scope.searchText;
-		$http.jsonp($scope.url)
-			.success(function(data, status, headers, config) {
+
+	$scope.categoriesResponse = [		// attempt
+		'Art',
+		'Business',
+		'Philosophy',
+		'Humor',
+		'Science'
+	]
+	$scope.letters = [
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 
+	'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z']
+
+	$scope.categories = $scope.categoriesResponse;  // attempt
+
+	$scope.categoryRequest = function(index) {
+		$scope.index = index;
+		$scope.clearTrue();
+		$scope.categoryUrl = 'http://localhost:8000/library/bookSearch?callback=JSON_CALLBACK&bookName=' + $scope.categoriesResponse[$scope.index];
+		console.log($scope.categoryUrl);
+		$http.jsonp($scope.categoryUrl)
+			.success(function(data, status, headers, config) {	
+				//
+			})
+			.error (function(data, status, headers, config) {
+				console.log("error");
+			})
+	}
+
+		// default path
+	$scope.urlSearch = 'http://localhost:8000/library/bookSearch?callback=JSON_CALLBACK&bookName=';
+ 	$scope.searchRequest = function() {
+ 		$scope.clearTrue();
+ 		$scope.currentPath = $scope.paths.searchData;
+		$scope.fullUrl = $scope.urlSearch + $scope.searchText;
+		$http.jsonp($scope.fullUrl)
+			.success(function(data, status, headers, config) {	
 				console.log(data);
 				$scope.data = data;
+				$scope.status = status;
+				console.log($scope.data, "Hey");
 			})
 			.error (function(data, status, headers, config) {
 				console.log("error");
 				$scope.data = data || "Request failed";
 				$scope.status = status;
+				console.log($scope.data);
+
 			})
 	}
 
