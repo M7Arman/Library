f4f7ff772dcbdc8adcbdf100009d0628771a303d
M7Arman <student@zeus.(none)>
2014-06-24 17:03:18 +0400

    modify testRunner and index.html(added mockup link)

---
+++ src/test/request.js
@@ -0,0 +1,14 @@
+describe('Library project!', function () {
+	//var $httpBackend, injector;
+	var response = [{
+		"bookName": "Samvel",
+	}];
+
+	it('returns particular place', function () {
+		expect(true).toEqual(true);
+	});
+
+	it('search request', function() { });
+	it('category list request', function() { });
+	it('category request', function() { });
+});
